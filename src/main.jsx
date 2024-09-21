cd9d0e07de0e9a3c9bbc2ec54094ce6745d15142
Premkumar8926 <84018107+Premkumar8926@users.noreply.github.com>
2024-09-22 02:17:12 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,10 @@ import App from './App.jsx'
 import './index.css'
 import axios from "axios"
 
+// Set the base URL for API requests
+axios.defaults.baseURL = "https://password-reset-backend-23iq.onrender.com/api/user"; // Adjust according to your setup
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
